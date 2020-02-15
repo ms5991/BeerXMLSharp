2ce6913c73a26c790da609242cb03878898b64f8
Michael Schulz <mschulz5991@gmail.com>
2020-02-15 14:28:45 -0800

    Move exceptions to different namespace

---
+++ BeerXMLSharp.UnitTests/OM/BeerXMLEntityBaseTests.cs
@@ -0,0 +1,45 @@
+﻿using BeerXMLSharp.OM;
+using BeerXMLSharp.Serialization;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BeerXMLSharp.UnitTests.OM
+{
+    [TestClass]
+    public class BeerXMLEntityBaseTests
+    {
+        [TestMethod]
+        [ExpectedException(typeof(BeerXMLInvalidObjectException))]
+        public void GetBeerXML_Invalid_Exception()
+        {
+            Mock<BeerXMLEntityBase> entity = new Mock<BeerXMLEntityBase>();
+
+            entity.Setup(e => e.IsValid(ref It.Ref<ValidationCode>.IsAny)).Returns(false);
+
+            entity.Object.GetBeerXML();
+        }
+
+        [TestMethod]
+
+        public void Serializer_SerializeCalled()
+        {
+            Mock<BeerXMLEntityBase> entity = new Mock<BeerXMLEntityBase>();
+
+            entity.Setup(e => e.IsValid(ref It.Ref<ValidationCode>.IsAny)).Returns(true);
+
+            Mock<IBeerXMLSerializer> serializer = new Mock<IBeerXMLSerializer>();
+
+            string result = "result";
+            serializer.Setup(s => s.Serialize(It.IsAny<IBeerXMLEntity>())).Returns(result);
+
+            entity.Object.Serializer = serializer.Object;
+
+            string serializedResult = entity.Object.GetBeerXML();
+
+            serializer.Verify(s => s.Serialize(entity.Object));
+        }
+    }
+}
---
+++ BeerXMLSharp/BeerXMLExceptions.cs
@@ -0,0 +1,77 @@
+﻿using BeerXMLSharp.OM;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BeerXMLSharp
+{
+    /// <summary>
+    /// Exception thrown when attempting to serialize an invalid IBeerXMLEntity
+    /// </summary>
+    /// <seealso cref="System.Exception" />
+    public class BeerXMLInvalidObjectException : Exception
+    {
+        /// <summary>
+        /// Gets the error code.
+        /// </summary>
+        /// <value>
+        /// The error code.
+        /// </value>
+        public ValidationCode ErrorCode { get; }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="BeerXMLInvalidObjectException"/> class.
+        /// </summary>
+        /// <param name="errorCode">The error code.</param>
+        public BeerXMLInvalidObjectException(ValidationCode errorCode) : base()
+        {
+            this.ErrorCode = errorCode;
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="BeerXMLInvalidObjectException"/> class.
+        /// </summary>
+        /// <param name="message">The message.</param>
+        /// <param name="errorCode">The error code.</param>
+        public BeerXMLInvalidObjectException(ValidationCode errorCode, string message) : base(message)
+        {
+            this.ErrorCode = errorCode;
+        }
+    }
+
+    /// <summary>
+    /// Exception thrown when deserialization encounters an XML element
+    /// with no corresponding type
+    /// </summary>
+    /// <seealso cref="System.Exception" />
+    public class BeerXMLUnknownTypeTagException : Exception
+    {
+        /// <summary>
+        /// Gets the name of the tag.
+        /// </summary>
+        /// <value>
+        /// The name of the tag.
+        /// </value>
+        public string TagName { get; }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="BeerXMLUnknownTypeTagException"/> class.
+        /// </summary>
+        /// <param name="tagName">Name of the tag.</param>
+        public BeerXMLUnknownTypeTagException(string tagName) : base()
+        {
+            this.TagName = tagName;
+        }
+
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="BeerXMLUnknownTypeTagException"/> class.
+        /// </summary>
+        /// <param name="tagName">Name of the tag.</param>
+        /// <param name="message">The message.</param>
+        public BeerXMLUnknownTypeTagException(string tagName, string message) : base(message)
+        {
+            this.TagName = tagName;
+        }
+    }
+}
